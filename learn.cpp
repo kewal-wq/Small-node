ed10598217f502a1c8ff6efefb3f29948f85b68f
kewal-wq <kewal.delhiwala5@gmail.com>
2022-03-19 23:47:09 +0530

    First Commit

---
+++ learn.cpp
@@ -0,0 +1,35 @@
+//The first block of a blockchain is called genuses block and it does not have a prev hash
+//SHA256 it is secure hasing algorithm (64 characters) hexadecimal 1 character takes 4 bytes
+//So total 256 bytes of memory in the computer
+//Immutable ledger (Record book)
+//Distributed P2P network where the whole set of data is stored in all the computers in that network
+//So that if there is change in any one all the systems would help in restoring the data very quickly
+//Mining is all about predicting the nounce which would generate a hash that would be below the target
+//And would help in creating a block
+//Byzantine fault tolerence (castle and the commander story)(Less than 33.3% can be traitors)
+//Consensius protocol
+//1) To check if the block added by the miner is not malicius by checking proof of work
+//2) To decide which block would be added first in simultaneous addtion of blocks.
+//The block which would be spread in more than half of the network would be added first
+
+
+//Ethereum is a network made on which we can run our smart contracts 
+//Each computer in the network will have the copy of that smart contract 
+//In this each node will contain
+//1)History of the transactions/History of the smart contract/Current state of the smart contract
+//This blockchain is just like a virtual memory in everyones device soo that sending malicious code wont affect
+//files of others and also there is a gas fees over several operations performed in the ethereum
+
+//DAO(Decentralized autonomous organisations which can run on itself without involving ppl)
+
+//Soft fork the whole contract will update wheraes in the hard fork there would be two parellel networks created
+//Which means that one has to update in order to be a part of any
+
+//Ethereum network can be used to store money or data, networks are formed by one or more nodes
+//Each node is a machine running on ethereum client, each node can contain full copy of the blockchain
+//Blockchain is a database which keeps record of the all the trasactions which may ever have taken place
+
+//In ethereum one account is used on all the networks
+//It takes time to mine the blocks ...if it takes more time than it will raise the target value 
+//And accordingly when it finds a nounce which creates a hash less than the target it will make a block
+//The time taken from 0 to the target nonce is known as block time
