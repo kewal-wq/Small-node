e40ebe2c063ee02301851a1f89088e36c02fd437
kewal-wq <kewal.delhiwala5@gmail.com>
2022-03-22 11:13:26 +0530

    Add mocha test

--- inbox/compile.js
+++ inbox/compile.js
@@ -1,1 +1,9 @@
 // compile code will go here
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const InboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(InboxPath, 'utf-8');
+
+module.exports =  solc.compile(source, 1).contracts[':Inbox'];
